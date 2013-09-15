9ef1f2c6d6f4ae62a103361ec4786f5aa6c1843d
Fotios Sioutis <sfotis@gmail.com>
2013-09-15 22:04:54 +0300

    Corrected FSF address in files.

--- src/Lib3DS/Lib3DS_tcb.h
+++ src/Lib3DS/Lib3DS_tcb.h
@@ -18,7 +18,7 @@
  *
  * You should  have received  a copy of the GNU Lesser General Public License
  * along with  this program;  if not, write to the  Free Software Foundation,
- * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * $Id: Lib3DS_tcb.h,v 1.11 2007/06/20 17:04:09 jeh Exp $
  */
